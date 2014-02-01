6af9eaff30bb13dd3e93a7ff606384fbf8c993df
biwa <boris@d00m.info>
2014-02-01 15:09:52 +0100

    Changed assembly name to ThreeDFloorMode.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die mit einer Assembly verknüpft sind.
-[assembly: AssemblyTitle("ThreeDFloorHelper")]
+[assembly: AssemblyTitle("ThreeDFloorMode")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ThreeDFloorHelper")]
+[assembly: AssemblyProduct("ThreeDFloorMode")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
