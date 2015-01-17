51e7b8cc6b486b9c08ed4ddd19c000af86b1c178
biwa <foracc@d00m.info>
2015-01-17 21:37:42 +0100

    - Added missing file SlopeVertexGroup.cs

---
+++ SlopeVertexGroup.cs
@@ -0,0 +1,36 @@
+﻿#region ================== Namespaces
+
+using System.Collections.Generic;
+using CodeImp.DoomBuilder.Geometry;
+
+#endregion
+
+namespace CodeImp.DoomBuilder.ThreeDFloorMode
+{
+	public class SlopeVertexGroup
+	{
+		#region ================== Variables
+
+		private List<SlopeVertex> vertices;
+		private int id;
+
+		#endregion
+
+		#region ================== Constructors
+
+		public SlopeVertexGroup(int id, List<SlopeVertex> vertices)
+		{
+			this.vertices = vertices;
+			this.id = id;
+		}
+
+		#endregion
+
+		#region ================== Properties
+
+		public List<SlopeVertex> Vertices { get { return vertices; } set { vertices = value; } }
+		public int Id { get { return id; } }
+
+		#endregion
+	}
+}
